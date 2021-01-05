01df706e3bfa63be04c74e8b93c30d0c012866c9
felecanvlad <74371101+felecanvlad@users.noreply.github.com>
2021-01-05 03:29:41 +0200

    remove team member

--- functions.js
+++ functions.js
@@ -12,8 +12,8 @@ const API = {
         METHOD: "GET"
     },
     DELETE: {
-        URL: "",
-        METHOD: "GET"
+        URL: "delete.json",
+        METHOD: "GET" // DELETE
     }
 };
 
@@ -28,15 +28,17 @@ function getPersonsHtml(persons) {
 }
 function getPersonHtml(person) {
     const gitHub = person.gitHub;
-    return `
+    return `<tr>
     <td>${person.firstName}</td>
     <td>${person.lastName}</td>
     <td>
         <a target="_blank" href="https://github.com/${gitHub}">GitHub</a>
         <a target="_blank" href="https://www.linkedin.com/">Linkedin</a>
     </td>
-    <td></td>
-    </tr>`
+    <td>
+        <a href="${API.DELETE.URL}?id=${person.id}">&#10006;</a>
+    </td>     
+</tr>`;
 }
 let allPersons = [];
 
@@ -90,10 +92,9 @@ function saveTeamMember() {
         .then(r => {
             console.warn(r);
             if (r.success) {
-                setTimeout(() => {
+                alert('saving data..., please wait until er are ready.');
                     console.info('refresh list');
                     loadList();
-                }, 30000);
             }
         });
 };
