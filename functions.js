796e5d28fc06c5d9b4c5acf2b5aafc534d722138
felecanvlad <74371101+felecanvlad@users.noreply.github.com>
2021-01-18 21:17:57 +0200

    add edit happy flow

--- functions.js
+++ functions.js
@@ -17,6 +17,8 @@ const API = {
     }
 };
 
+let editId; 
+
 API.READ.URL
 
 function insertPersons(persons) {
@@ -36,7 +38,7 @@ function getPersonHtml(person) {
     </td>
     <td>
         <a href="#" class = "delete-row" data-id = "${person.id}">&#10006;</a>
-        
+        <a href="#" class = "edit-row" data-id = "${person.id}">&#9998;</a>
     </td>
 </tr>`;
 }
@@ -91,8 +93,36 @@ function saveTeamMember() {
         });
 };
 
-function deleteTeamMember (id) {
-    console.warn('delete',id);
+function updateTeamMember() {
+    const firstName = document.querySelector("input[name=firstName]").value;
+    const lastName = document.querySelector("input[name=lastName]").value;
+    const gitHub = document.querySelector("input[name=gitHub]").value;
+    
+    const person = {
+        id: editId,
+        firstName, 
+        lastName, 
+        gitHub: gitHub
+    }
+    console.info('updateing...', person,JSON.stringify(person));
+
+    fetch(API.UPDATE.URL, {
+        method: API.UPDATE.METHOD,
+        headers: {
+            "Content-Type": "application/json"
+          },
+        body: API.UPDATE.METHOD === "GET" ? null : JSON.stringify(person)
+    })
+        .then(res => res.json())
+        .then(r => {
+            console.warn(r);
+            if (r.success) {
+                loadList();
+            }
+        });
+};
+
+function deleteTeamMember (id) { 
     fetch(API.DELETE.URL, {
         method: "API.DELETE.METHOD",
         headers: {
@@ -109,6 +139,20 @@ function deleteTeamMember (id) {
             });
 }
 
+function populateCurrentMember (id) {
+    var person = allPersons.find(person => person.id === id);
+
+    editId = id; 
+
+    const firstName = document.querySelector("input[name=firstName]");
+    const lastName = document.querySelector("input[name=lastName]");
+    const gitHub = document.querySelector("input[name=gitHub]");
+
+    firstName.value = person.firstName;
+    lastName.value = person.lastName;
+    gitHub.value = person.gitHub;
+}
+
 function addEventListeners () {
     const search = document.getElementById ('search');
     search.addEventListener("input", e => {
@@ -121,7 +165,12 @@ function addEventListeners () {
 
     const saveBtn = document.querySelector("#list tfoot button");
     saveBtn.addEventListener("click", e => {
-        saveTeamMember();
+        if (editId) {
+            updateTeamMember();
+        } else {
+            saveTeamMember();
+        }
+        
     });
 
     const table = document.querySelector("#list tbody");
@@ -130,6 +179,9 @@ function addEventListeners () {
         if (target.matches("a.delete-row")) {
             const id = target.getAttribute("data-id");
             deleteTeamMember(id);
+        } else if (target.matches("a.edit-row")) {
+            const id = target.getAttribute("data-id");
+            populateCurrentMember(id);
         }
     });
 }
